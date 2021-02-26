0af1127bcf9447be13ab9eb588e453adc2a8de32
RakMont <nabogaria@gmail.com>
2021-02-26 09:44:56 -0400

    first commit

---
+++ workshop-api/Controllers/GroupController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace workshop_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroupController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetAllGroups()
+        {
+
+        }
+    }
+}
